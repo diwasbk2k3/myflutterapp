caf110bc75a90aa213943105ea4935433f65280c
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-07 00:06:03 +0545

    App Bar

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:myflutterapp/screens/first_screen.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: FirstScreen(),
+    );
   }
 }
---
+++ lib/screens/first_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text(
+          "WhatsApp",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
