deb722a65d862797aba2b3e04cca0befc3551563
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-06 23:47:21 +0545

    App Setup

---
+++ android/app/src/main/kotlin/com/diwas/np/myflutterapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.diwas.np.myflutterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:myflutterapp/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
