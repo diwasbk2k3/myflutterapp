bae771f9711ee8cc195cc86f2a5cea771904c9d3
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-07 00:32:57 +0545

    Local Image

--- lib/screens/image_view_screen.dart
+++ lib/screens/image_view_screen.dart
@@ -15,7 +15,8 @@ class ImageViewScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Image.network("https://th.bing.com/th/id/R.195fcd8a7a2171e72cfb09df8c82204a?rik=JEnEi2nNfwf5sA&riu=http%3a%2f%2fhdqwalls.com%2fwallpapers%2fsunset-beach-cp.jpg&ehk=6yg0sCNVC4Q2s8xrrgXZ5XIvJLpmkRc1eEBq%2bAk%2fUPE%3d&risl=&pid=ImgRaw&r=0")
+          Image.network("https://th.bing.com/th/id/R.195fcd8a7a2171e72cfb09df8c82204a?rik=JEnEi2nNfwf5sA&riu=http%3a%2f%2fhdqwalls.com%2fwallpapers%2fsunset-beach-cp.jpg&ehk=6yg0sCNVC4Q2s8xrrgXZ5XIvJLpmkRc1eEBq%2bAk%2fUPE%3d&risl=&pid=ImgRaw&r=0"),
+          Image.asset("assets/images/camera.jpg")
         ],
       ),
     );
