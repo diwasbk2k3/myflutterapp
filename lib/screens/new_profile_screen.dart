d789169eb8886442a959a8d48ae00c0df0f6d6f2
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-08 23:36:26 +0545

    Profile Column

--- lib/screens/new_profile_screen.dart
+++ lib/screens/new_profile_screen.dart
@@ -9,13 +9,37 @@ class NewProfileScreen extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.deepPurple,
         centerTitle: true,
-        leading: Icon(Icons.home, color: Colors.white,),
-        title: Text("Diwas Info", style: TextStyle(
-          color: Colors.white,
-          fontWeight: FontWeight.bold
-        ),),
+        leading: Icon(Icons.home, color: Colors.white),
+        title: Text(
+          "Diwas Info",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.search), color: Colors.white)
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+            color: Colors.white,
+          ),
+        ],
+      ),
+
+      // Body
+      body: Column(
+        children: [
+          CircleAvatar(
+            radius: 40,
+            backgroundColor: Colors.deepPurple,
+            child: Text(
+              "D",
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 30,
+              ),
+            ),
+          ),
+          Text("Diwas Bk"),
+          Text("Hello, I am a Full Stack Developer with keen intereset in Data Science & Artificial Intelligence.")
         ],
       ),
     );
