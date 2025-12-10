5497917456ba3727b2770a006c1dee473f977d75
diwasbk2k3 <diwasbk2k3@gmail.com>
2025-12-10 16:04:48 +0545

    Bottom Screens

---
+++ lib/screens/bottom_screens/about_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AboutScreen extends StatelessWidget {
+  const AboutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Text("Welcome to About Screen."),
+    );
+  }
+}
